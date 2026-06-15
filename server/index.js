1617169ab39c6a13fcb2267699b9d2f2d779321a
kartikDevS <kartik9136@gmail.com>
2026-06-15 17:51:58 +0530

    fixing bugs

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ app.use(cors({
   credentials: true
 }))
 
+console.log("Connecting to URI:", process.env.MONGO_URI);
+
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("✅Mongo atlas connected successfully!"))
 .catch(err=>console.log('❌ MongoDB failed:', err.message))
@@ -52,45 +54,4 @@ app.listen(process.env.PORT,()=>console.log(`🚀 Server running on port ${proce
 
 
 
-
-
-// const dns = require('dns')
-// dns.setServers(['1.1.1.1', '8.8.8.8'])
-
-// require('dotenv').config()
-// const express = require('express')
-// const { createServer } = require('http')
-// const { Server } = require('socket.io')
-// const cors = require('cors')
-// const mongoose = require('mongoose')
-
-// const app = express()
-
-// app.use(cors())
-// app.use(express.json())
-
-// const httpServer = createServer(app)
-// const io = new Server(httpServer, {
-//   cors: { origin: '*' }
-// })
-
-// mongoose.connect(process.env.MONGO_URI)
-// .then(()=>console.log("✅Mongo atlas connected successfully!"))
-// .catch(err=>console.log('❌ MongoDB failed:', err.message))
-
-// app.get('/', (req, res) => {
-//   res.json({ status: 'Squad server is running 🔥' })
-// })
-
-// io.on('connection', (socket) => {
-//   console.log('🟢 A user connected:', socket.id)
-
-//   socket.on('disconnect', () => {
-//     console.log('🔴 User disconnected:', socket.id)
-//   })
-// })
-
-// httpServer.listen(process.env.PORT, () => {
-//   console.log(`🚀 Server running on port ${process.env.PORT}`)
-// })
 
